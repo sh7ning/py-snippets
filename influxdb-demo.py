46ffa4ff88fba180ed58b2fe84d61bad65ad9800
sh7ning <sh7ning@qq.com>
2021-02-20 00:27:52 +0800

    fix tz

--- influxdb-demo.py
+++ influxdb-demo.py
@@ -44,7 +44,8 @@ for i in range(1, 60*24*7):     # 60*24*7
         "fields": {     # 数据
             "value": random.randint(160, 400) / 100
         },
-        "time": int(t.timestamp())      # 建议用这个
+        # "time": int(t.timestamp())     # 这个针对 time_precision=s
+        "time": int(t.timestamp() / 60)  # 建议用这个，这个针对 time_precision=m
         # "time": t   # 这个用法要小心，主要是时区问题，比如只写入当前时间一条记录，然后查询不到，时区问题。。。
     })
 
@@ -57,7 +58,7 @@ influx.client.drop_database(influx_db_name)     # TODO
 influx.select_db(influx_db_name)
 
 # print(influx.client.get_list_retention_policies())
-influx.client.write_points(json_body, batch_size=100, time_precision='s')   # 这里的 s 调整会影响具体数据，不过只对 time 为 datetime 类型生效， time 为 int 的时候应该为 s
+influx.client.write_points(json_body, time_precision='m')   # 这里的 s 调整会影响具体数据，不过只对 time 为 datetime 类型生效， time 为 int 的时候应该为 s
 
 sql = 'SELECT distinct("value") FROM "30d_rp"."rate_history" WHERE time >= now() - 1h GROUP BY time(1m) fill(null);'
 result = influx.client.query(sql)
